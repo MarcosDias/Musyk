d78e9e4ab8bc1f507dd9649b3791595c1c1e89df
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-08-02 15:26:06 -0300

    script sql instrumentos

---
+++ src/main/resources/musykScript.sql
@@ -0,0 +1,33 @@
+INSERT INTO instrumento (nome) VALUES ('Alaúde'), ('Baixo'), ('Bandolim'),
+('Banjo'), ('Bateria'), ('Castanholas'), ('Cavaquinho'), ('Clarinete'), 
+('Contrabaixo'), ('Cravo'), ('Dobro'), ('Eufónio'), ('Flauta'), ('Fliscorne'), 
+('Gaita'), ('Gaita de Fólio'), ('Guitarra Elétrica'), ('Harpa'), ('Lira'), 
+('Maraca'), ('Melódica'), ('Oboé'), ('Ocarina'), ('Pandeiro'), ('Piano'), 
+('Quissange'), ('Rabeca'), ('Reco-reco'), ('Sanfona'), ('Saxofone'), ('Tambor'), 
+('Trompete'), ('Uffataha'), ('Viola'), ('Violino'), ('Violoncelo'), ('Violão'), 
+('Voz');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
